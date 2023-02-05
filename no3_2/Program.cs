949cbf555d71e7ac36df2e134e7b4aed09df8c0a
Thitiwudh Uasmith <thitiwudh.u@mail.kmutt.ac.th>
2023-02-05 22:55:49 +0700

    feat: Added no3_2 answer.

---
+++ no3_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static float f(float x)
+    {
+        if(x > -1 && x <= 0)
+        {
+            return (float)Math.Sqrt(1-x*x);
+        }
+        else if(x > 0 && x <= 1)
+        {
+            return -(float)Math.Sqrt(1-x*x);
+        }
+        else if(x <= -1)
+        {
+            return f(x+2);
+        }
+        else
+        {
+            return f(x-2);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        float x = float.Parse(Console.ReadLine());
+        Console.WriteLine(f(x));
+    }
+}
