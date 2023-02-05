a2d0203e8ae8789b644de62ba6db7a6cf54b063d
Thitiwudh Uasmith <thitiwudh.u@mail.kmutt.ac.th>
2023-02-05 22:46:06 +0700

    feat: Added no1_1 answer.

---
+++ no1_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static float f(float x)
+    {
+        if(x*x <= 4)
+        {
+            return 2;
+        }
+        else
+        {
+            return 1 + f(x - 4*Math.Sign(x));
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        float x = float.Parse(Console.ReadLine());
+        Console.WriteLine(f(x));
+    }
+}
